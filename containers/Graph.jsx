9f71d23bab9463738f9d6dbdad165bb04ca6aacf
Michael Neveu <mbneveu@gmail.com>
2017-12-02 10:44:07 -0800

    graphs working

--- containers/Graph.jsx
+++ containers/Graph.jsx
@@ -10,9 +10,7 @@ class Graph extends React.Component {
       status: 'primaryOnly',
       focus: 'monthly',
       cleanData: this.props.graphData,
-      weeklyData: this.props.graphData.slice(0, 12),
-      monthlyData: this.props.graphData.slice(0, 12),
-      quarterlyData: this.props.graphData.slice(0, 4),
+      
     };
   }
 
@@ -22,6 +20,7 @@ class Graph extends React.Component {
 
 
   
+
   render() {
     //////////
  
@@ -77,47 +76,167 @@ class Graph extends React.Component {
     //   return answer;
     // };
 
-    var makeTimeFocusedData = function(countWeeks, data) {
-   
-      var answer = [];
-      var tempPeriod = [];
-      var tempPeriodFormatted = {};
-      var tempTotal = 0;
-      var averageTemp = 0;
-      var counter = 0;
-      
-      if(data.length === undefined) {return data}
-      for (var i = 0; i < data.length; i ++) {
-          tempPeriod.push(presentationData[i]);
-          tempTotal += data[i].primaryTrendVolume;
-          counter +=1;
-          //console.log(counter);
-          
-        if (counter === countWeeks ) {
-          tempPeriodFormatted.primaryTrendVolume = tempTotal/countWeeks;
-          tempPeriodFormatted.date = tempPeriod[0].date;
-          answer.push(tempPeriodFormatted);
-          tempPeriodFormatted = {};
-          tempPeriod = [];
-          tempTotal = 0;
-          counter = 0;
+
+    //CREATE 2 CLEAN DATASETS
+
+    var dataCleanerMovie1 = function(data) {
+      var cleanArray = [];
+      for (var i = 0; i < data.length ; i++) {
+        if (data[i] && data[i].primaryTrendVolume !== undefined) {
+          var temp = {}
+          temp.date = data[i].date
+          temp.primaryTrendVolume = data[i].primaryTrendVolume
+          cleanArray.push(temp);
         }
       }
-      console.log('AVERAGES = ', answer)
-      return answer;
+      return cleanArray;
+    }
+
+    var dataCleanerMovie2 = function(data) {
+      var cleanArray = [];
+      for (var i = 0; i < data.length ; i++) {
+        if (data[i] && data[i].secondaryTrendVolume !== undefined) {
+          var temp = {}
+          temp.date = data[i].date
+          temp.secondaryTrendVolume = data[i].secondaryTrendVolume
+          cleanArray.push(temp);
+        }
+      }
+      return cleanArray;
+    }
+
+    console.log('GRAPH DATA BEING CLEANED 1 2 = ', this.props.graphData)
+    var Movie1CleanData = dataCleanerMovie1(this.props.graphData);
+    console.log('CLEAN MOVIE 1 DATA = ', Movie1CleanData);
+    var Movie2CleanData = dataCleanerMovie2(this.props.graphData);
+    console.log('CLEAN MOVIE 2 DATA = ', Movie2CleanData);
+
+
+
+
+
+    //TEST
+    //var usedTempData = [];
+    //console.log('HERE!!!! USEDTEMPDATA = ', usedTempData)
+    //console.log('1233456789', this.props.graphData)
+    var zipperInMovies = function (movie1Data, movie2Data) {
+      // if (data.length > 1) {
+      //   usedTempData = data.slice(0, 52);
+      //   //console.log('HERE!!!! USEDTEMPDATA 1 MOVE = ', usedTempData)
+      //   return data.slice(0, 52);
+      // }
+      // if (data.length > 52) {
+      //   usedTempData = data.slice(53, 104);
+      //   //console.log('HERE!!!! USEDTEMPDATA 2 MOVIES = ', usedTempData)
+      //   return data.slice(54, 104);
+      // }
+
+
+      if (movie2Data.length < 1) { return movie1Data }
+      if(movie1Data.length > 1 && movie2Data.length > 1) {
+        var cleanCombinedMovieData = [];
+        var temp = {};
+        for (var i = 0; i < 52; i ++) {
+          temp.date = movie1Data[i].date;
+          temp.primaryTrendVolume = movie1Data[i].primaryTrendVolume;
+          temp.secondaryTrendVolume = movie2Data[i].secondaryTrendVolume;
+          cleanCombinedMovieData.push(temp);
+          temp = {};
+        }
+        return cleanCombinedMovieData;
+      }
+
+
+      return movie1Data;
+    }
+
+    
+    var sortedBetweenMoveiesData = [...zipperInMovies(Movie1CleanData, Movie2CleanData)];
+    console.log('COMBINED CLEAN DATA PERFECT!!! = ', sortedBetweenMoveiesData);
+
+    var context = this;
+    var makeTimeFocusedData = function(countWeeks, data) {
+      //console.log('AAAAAA123 = ', data);
+      var answer = [];
+  var tempPeriod = [];
+  var tempPeriodFormatted = {};
+  var tempTotal = 0;
+  var tempTotal2 = 0;
+  var tempSecond = 0;
+  var tempTotalSecond = 0;
+  var averageTemp = 0;
+  var counter = 0;
+  var periodCounter = 1;
+      
+      if(data.length === undefined) {return data}
+      if (data[0] && data[0].secondaryTrendVolume === undefined) {
+        for (var i = 0; i < data.length; i ++) {
+            tempPeriod.push(data[i]);
+            tempTotal += data[i].primaryTrendVolume;
+            counter +=1;
+            //console.log(counter);
+            
+          if (counter === countWeeks ) {
+            if(context.state.focus === 'monthly' && periodCounter === 13) {
+              break;
+            }
+            tempPeriodFormatted.primaryTrendVolume = tempTotal/countWeeks;
+            tempPeriodFormatted.date = periodCounter;
+            //tempPeriodFormatted.date = tempPeriod[0].date;
+            answer.push(tempPeriodFormatted);
+            tempPeriodFormatted = {};
+            tempPeriod = [];
+            tempTotal = 0;
+            counter = 0;
+            periodCounter +=1;
+          }
+        }
+        //console.log('AVERAGES PART 1 RAN OF SORTER= ', answer)
+        return answer;
+      }
+
+      ///HERE!!!
+        if (data[0] && data[0].hasOwnProperty("secondaryTrendVolume")) {
+    for (var i = 0; i < data.length; i ++) {
+        tempPeriod.push(data[i]);
+        tempTotal += data[i].primaryTrendVolume;
+        tempTotal2 += data[i].secondaryTrendVolume;
+        counter +=1;
+        //console.log(counter);
+        
+      if (counter === countWeeks ) {
+        // if(context.state.focus === 'monthly' && periodCounter === 13) {
+        //   break;
+        // }
+        tempPeriodFormatted.primaryTrendVolume = tempTotal/countWeeks;
+        tempPeriodFormatted.secondaryTrendVolume = tempTotal2/countWeeks;
+        tempPeriodFormatted.date = periodCounter;
+        //tempPeriodFormatted.date = tempPeriod[0].date;
+        answer.push(tempPeriodFormatted);
+        tempPeriodFormatted = {};
+        tempPeriod = [];
+        tempTotal = 0;
+        counter = 0;
+        periodCounter +=1;
+      }
+    }
+    //console.log('AVERAGES PART 1 RAN OF SORTER= ', answer)
+    return answer;
+  }
+      return data;
     };
 
     
-    var presentationData = [...this.props.graphData];
+    var presentationData = sortedBetweenMoveiesData;
     if(this.state.focus === 'weekly') {
-      presentationData = makeTimeFocusedData(1, this.props.graphData);
+      presentationData = makeTimeFocusedData(1, sortedBetweenMoveiesData);
     }
     if(this.state.focus === 'monthly') {
-      presentationData = makeTimeFocusedData(4, this.props.graphData);
+      presentationData = makeTimeFocusedData(4, sortedBetweenMoveiesData);
     }
     if(this.state.focus === 'quarterly') {
-      presentationData = makeTimeFocusedData(12, this.props.graphData);
-      console.log('DATA YOU ARE USING = ', presentationData);
+      presentationData = makeTimeFocusedData(12, sortedBetweenMoveiesData);
+      //console.log('DATA YOU ARE USING = ', presentationData);
     }
     // this.props.graphData[0] ? console.log('GRAPHDATA SAMPLE 1234 = ', this.props.graphData[0]): console.log('not there')
     // this.props.primaryMovie.releaseDate ? console.log('RELEASE & TRENDDATA SAMPLE 1234 = ', this.props.primaryMovie.releaseDate, this.props.primaryMovie.trendData[0]): console.log('not there')
@@ -128,8 +247,8 @@ class Graph extends React.Component {
       
       return (
         <div id="graph">
-        {this.props.graphData[0] ? console.log('GRAPHDATA SAMPLE 456 = ', this.props.graphData[0]): console.log('not there')}
-        {this.props.primaryMovie.releaseDate ? console.log('RELEASE & TREND DATA SAMPLE 1234 = ', this.props.primaryMovie.releaseDate, this.props.primaryMovie.trendData[0]): console.log('not there')}
+        {/*this.props.graphData[0] ? console.log('GRAPHDATA SAMPLE 456 = ', this.props.graphData[0]): console.log('not there')*/}
+        {/*this.props.primaryMovie.releaseDate ? console.log('RELEASE & TREND DATA SAMPLE 1234 = ', this.props.primaryMovie.releaseDate, this.props.primaryMovie.trendData[0]): console.log('not there')*/}
         {/*console.log('PRESENTATION DATA USED IN RENDER FUNCTION = ', presentationData)*/} 
           <LineChart width={900} height={400} data={presentationData}>
             <XAxis dataKey="date"/>
@@ -160,6 +279,7 @@ class Graph extends React.Component {
             <Tooltip />
             <Legend verticalAlign="top" />
           </LineChart>*/}
+          {this.props.graphData && this.props.primaryMovie.title? <div>{this.props.primaryMovie.title} Release Date: {this.props.primaryMovie.releaseDate}</div> : ''}
         </div>
       );
     } else {
