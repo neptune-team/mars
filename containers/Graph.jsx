4cc3b039fe345a95d1663da890d953d67ba27bbb
EnkhtushigNamkhai <enkhtushig@gmail.com>
2017-12-02 20:55:00 -0800

    Adding the fix ifi made to graph

--- containers/Graph.jsx
+++ containers/Graph.jsx
@@ -1,43 +1,3 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
-// import { connect } from 'react-redux';
-// import { LineChart, Line, CartesianGrid, XAxis, YAxis, Label, Tooltip, Legend } from 'recharts';
-
-// function Graph(props) {
-//   return (
-//     <div id="graph">
-//       <LineChart width={1000} height={400} data={props.graphData}>
-//         <Line name={props.primaryMovie.title || ' '} type="monotone" dataKey="primaryTrendVolume" stroke="#8884d8" />
-//         <Line name={props.secondaryMovie.title || ' '} type="monotone" dataKey="secondaryTrendVolume" stroke="#FF0000" />
-//         <CartesianGrid stroke="#ccc" />
-//         <XAxis dataKey="date">
-//           <Label value="Date" offset={0} position="insideBottom" />
-//         </XAxis>
-//         <YAxis label={{ value: 'Relative Search Volume', angle: -90, position: 'insideLeft' }} />
-//         <Tooltip />
-//         <Legend verticalAlign="top" />
-//       </LineChart>
-//     </div>
-//   );
-// }
-
-// Graph.propTypes = {
-//   graphData: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
-//   primaryMovie: PropTypes.shape({
-//     title: PropTypes.string,
-//   }).isRequired,
-//   secondaryMovie: PropTypes.shape({
-//     title: PropTypes.string,
-//   }).isRequired,
-// };
-
-// function mapStateToProps({ graphData }) {
-//   return { graphData };
-// }
-
-// export default connect(mapStateToProps)(Graph);
-
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
@@ -175,7 +135,7 @@ class Graph extends React.Component {
       if(movie1Data.length > 1 && movie2Data.length > 1) {
         var cleanCombinedMovieData = [];
         var temp = {};
-        for (var i = 0; i < 52; i ++) {
+        for (var i = 0; i < Math.min(52, movie1Data.length, movie2Data.length); i ++) {
           temp.date = movie1Data[i].date;
           temp.primaryTrendVolume = movie1Data[i].primaryTrendVolume;
           temp.secondaryTrendVolume = movie2Data[i].secondaryTrendVolume;
